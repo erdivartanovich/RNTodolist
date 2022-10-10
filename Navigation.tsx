b125e706be17e868ff71dc918fed4c666fab0342
erdivartanovich <erdivartanovich@gmail.com>
2022-10-11 04:39:10 +0700

    fix navigation

--- Navigation.tsx
+++ Navigation.tsx
@@ -1,4 +1,3 @@
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./pages/home/Home";
 import TodoDetail from "./pages/todo-detail/TodoDetail";
@@ -15,32 +14,30 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const Navigation = () => (
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="TodoList"
-        component={TodoList}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="TodoDetail"
-        component={TodoDetail}
-        options={({ route }) => ({
-          title: route.params.title,
-          headerStyle: {
-            backgroundColor: "#DA6600",
-          },
-          headerTintColor: "#fff",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-        })}
-      />
-    </Stack.Navigator>
-  </NavigationContainer>
+  <Stack.Navigator initialRouteName="Home">
+    <Stack.Screen
+      name="Home"
+      component={Home}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="TodoList"
+      component={TodoList}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="TodoDetail"
+      component={TodoDetail}
+      options={({ route }) => ({
+        title: route.params.title,
+        headerStyle: {
+          backgroundColor: "#DA6600",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      })}
+    />
+  </Stack.Navigator>
 );
